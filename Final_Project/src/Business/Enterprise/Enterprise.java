116cd27566fb80c4db48a7cb3f498e3b646a6c1a
Bhumika Khatri <khatri.bh@husky.neu.edu>
2016-11-26 12:20:26 -0500

    Enterprise Types added

--- Final_Project/src/Business/Enterprise/Enterprise.java
+++ Final_Project/src/Business/Enterprise/Enterprise.java
@@ -15,7 +15,30 @@ import Business.Organization.OrganizationDirectory;
 public abstract class Enterprise extends Organization{
     
     private OrganizationDirectory organizationDirectory;
+    private EnterpriseType enterpriseType;
+    
+    
+    public enum EnterpriseType{
+        MainCenter("MainCenter"), InventoryCenter("InventoryCenter"), CompostingCenter("CompostingCenter"), TransportCenter("TransportCenter");
+        
+        private String value;
+        private EnterpriseType(String value){
+            this.value=value;
+        }
+        public String getValue() {
+            return value;
+        }
 
+        public void setValue(String value) {
+            this.value = value;
+        }
+        @Override
+        public String toString(){
+            return value;
+        }
+    }
+
+    
     public OrganizationDirectory getOrganizationDirectory() {
         return organizationDirectory;
     }
