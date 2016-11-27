a593b43f9aaf7a9899761a394dae48b250b9a648
Aadesh Randeria <randeria.a@husky.neu.edu>
2016-11-27 03:40:02 -0500

    transport manager jpanel

--- Final_Project/src/userinterface/LabManagerRole/LabManagerWorkAreaJPanel.java
+++ Final_Project/src/userinterface/LabManagerRole/LabManagerWorkAreaJPanel.java
@@ -28,23 +28,23 @@ public class LabManagerWorkAreaJPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        btnManageLabAssistant = new javax.swing.JButton();
+        btnUserAccount = new javax.swing.JButton();
+        btnWorkQueue = new javax.swing.JButton();
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel1.setText("My Work Area - Lab Manager Role");
 
-        jButton1.setText("Manage Lab Assistant");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnManageLabAssistant.setText("Manage Lab Assistant");
+        btnManageLabAssistant.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnManageLabAssistantActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Manage User Account");
+        btnUserAccount.setText("Manage User Account");
 
-        jButton3.setText("Manage Work Queue");
+        btnWorkQueue.setText("Manage Work Queue");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -54,13 +54,13 @@ public class LabManagerWorkAreaJPanel extends javax.swing.JPanel {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3))
+                    .addComponent(btnManageLabAssistant)
+                    .addComponent(btnUserAccount)
+                    .addComponent(btnWorkQueue))
                 .addContainerGap(319, Short.MAX_VALUE))
         );
 
-        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jButton1, jButton2, jButton3});
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {btnManageLabAssistant, btnUserAccount, btnWorkQueue});
 
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -68,24 +68,24 @@ public class LabManagerWorkAreaJPanel extends javax.swing.JPanel {
                 .addGap(28, 28, 28)
                 .addComponent(jLabel1)
                 .addGap(32, 32, 32)
-                .addComponent(jButton1)
+                .addComponent(btnManageLabAssistant)
                 .addGap(18, 18, 18)
-                .addComponent(jButton2)
+                .addComponent(btnUserAccount)
                 .addGap(18, 18, 18)
-                .addComponent(jButton3)
+                .addComponent(btnWorkQueue)
                 .addContainerGap(313, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnManageLabAssistantActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageLabAssistantActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnManageLabAssistantActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton btnManageLabAssistant;
+    private javax.swing.JButton btnUserAccount;
+    private javax.swing.JButton btnWorkQueue;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
