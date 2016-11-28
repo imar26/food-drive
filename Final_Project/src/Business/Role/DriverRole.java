adfaf8f4d5badefff2b0c88aae9bceedd413222f
Aadesh Randeria <randeria.a@husky.neu.edu>
2016-11-27 23:36:34 -0500

    roles code updated

--- Final_Project/src/Business/Role/DriverRole.java
+++ Final_Project/src/Business/Role/DriverRole.java
@@ -10,6 +10,7 @@ import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
 import javax.swing.JPanel;
+import userinterface.TransportManagerRole.ManageDriverJPanel;
 
 /**
  *
@@ -19,8 +20,7 @@ public class DriverRole extends Role {
 
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        return null;
+        return new ManageDriverJPanel(userProcessContainer);
     }
     
 }
--- Final_Project/src/Business/Role/LabAssistantRole.java
+++ Final_Project/src/Business/Role/LabAssistantRole.java
@@ -10,6 +10,7 @@ import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
 import javax.swing.JPanel;
+import userinterface.LabManagerRole.ManageLabAssistantJPanel;
 
 /**
  *
@@ -19,8 +20,7 @@ public class LabAssistantRole extends Role {
 
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        return null;
+        return new ManageLabAssistantJPanel(userProcessContainer);
     }
     
 }
