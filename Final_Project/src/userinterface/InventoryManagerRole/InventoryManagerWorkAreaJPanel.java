b5f502ea1c093bbe5989c1bbe18c799a1fa3b758
Aadesh Randeria <randeria.a@husky.neu.edu>
2016-12-11 23:36:41 -0500

    code updated

--- Final_Project/src/userinterface/InventoryManagerRole/InventoryManagerWorkAreaJPanel.java
+++ Final_Project/src/userinterface/InventoryManagerRole/InventoryManagerWorkAreaJPanel.java
@@ -59,7 +59,7 @@ public class InventoryManagerWorkAreaJPanel extends javax.swing.JPanel {
         
         model.setRowCount(0);
         for (WorkRequest request : organization.getWorkQueue().getWorkRequestList()){
-            Object[] row = new Object[6];
+            Object[] row = new Object[5];
             row[0] = request;
             row[1] = request.getStatus();
             int quantity = ((FoodWorkRequest) request).getQuantity();
